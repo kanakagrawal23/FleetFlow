f793fd38afe6f352fabe4c7dfd4c7276e2484d42
KrishnaV2 <krishnavijaywargia007@gmail.com>
2026-02-21 13:49:23 +0530

    auth flow complete

--- app/dashboard/dashboard.tsx
+++ app/dashboard/dashboard.tsx
@@ -2,5 +2,13 @@
 import { authClient } from "@/lib/auth/auth-client";
 
 export default function Dashboard({ session }: { session: typeof authClient.$Infer.Session }) {
-	return <></>;
+	return <>
+		<iframe 
+			title="main" 
+			className="w-full h-[40%]"
+			src="https://app.powerbi.com/view?r=eyJrIjoiYjNhOTY1NmItNWE2Ni00YmQ4LWIyOTAtNmI0Y2JjYzNiMDM2IiwidCI6ImJhNGRhNDJmLWI4M2ItNGNiNy1hZDM2LTAxNzJkNzZhNzBhNyJ9" 
+			frameBorder={0} 
+			allowFullScreen={true}>
+		</iframe>
+	</>;
 }
