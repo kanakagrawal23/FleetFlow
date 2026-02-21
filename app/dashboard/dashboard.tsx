3ed24b493305951eaae52ec7f6ee7b897a4c8c4c
KrishnaV2 <krishnavijaywargia007@gmail.com>
2026-02-21 14:01:11 +0530

    main dashboard powerbi integration

--- app/dashboard/dashboard.tsx
+++ app/dashboard/dashboard.tsx
@@ -3,12 +3,16 @@ import { authClient } from "@/lib/auth/auth-client";
 
 export default function Dashboard({ session }: { session: typeof authClient.$Infer.Session }) {
 	return <>
+	<div 
+		className="w-full h-[300px] overflow-hidden relative"
+	>
 		<iframe 
 			title="main" 
-			className="w-full h-[40%]"
-			src="https://app.powerbi.com/view?r=eyJrIjoiYjNhOTY1NmItNWE2Ni00YmQ4LWIyOTAtNmI0Y2JjYzNiMDM2IiwidCI6ImJhNGRhNDJmLWI4M2ItNGNiNy1hZDM2LTAxNzJkNzZhNzBhNyJ9" 
+			className="w-full h-[370px] border-none"
+			src="https://app.powerbi.com/view?r=eyJrIjoiMWQ0YmYxODMtZGZjZC00ZWZhLWFhMzktNzE4N2E3OGE4NjhmIiwidCI6ImJhNGRhNDJmLWI4M2ItNGNiNy1hZDM2LTAxNzJkNzZhNzBhNyJ9"
 			frameBorder={0} 
 			allowFullScreen={true}>
 		</iframe>
+	</div>
 	</>;
 }
